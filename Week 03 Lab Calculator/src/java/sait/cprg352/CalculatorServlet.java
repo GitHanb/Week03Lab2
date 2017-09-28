8e67323bb0deccd7122ddb725336a56f4140c846
636334 <636334@ELF6IT215856.ACDM.DS.SAIT.CA>
2017-09-28 08:29:57 -0600

    Week 03 Lab 2
    modified the structure

--- Week 03 Lab Calculator/src/java/sait/cprg352/CalculatorServlet.java
+++ Week 03 Lab Calculator/src/java/sait/cprg352/CalculatorServlet.java
@@ -20,7 +20,43 @@ public class CalculatorServlet extends HttpServlet {
    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException 
+    {
+        
+        
+        String firstStr = request.getParameter("first");
+        String secondStr = request.getParameter("second");
+        String operation = request.getParameter("operation");
+    
+        int result=0;
+        boolean calcPerformed=false;
+    
+        if (operation!=null)
+        {
+            if (firstStr!=null && secondStr!=null && !firstStr.equals("") && !secondStr.equals(""))
+            {
+                int first = Integer.parseInt(firstStr);
+                int second = Integer.parseInt(secondStr);
+
+                char operationType = operation.charAt(0);
+
+                switch (operationType)
+                {
+                    case '+': result = first + second;
+                        break;
+                    case '-': result = first - second;
+                        break;
+                    case '*': result = first * second;
+                        break;
+                    case '%': result = first % second;
+                        break;
+                }
+
+                calcPerformed = true;
+                request.setAttribute("result", result);
+                
+            }
+        }
         getServletContext().getRequestDispatcher("/WEB-INF/Calculator.jsp").forward(request, response);
     }
 
--- Week 03 Lab Calculator/web/WEB-INF/Calculator.jsp
+++ Week 03 Lab Calculator/web/WEB-INF/Calculator.jsp
@@ -1,39 +1,3 @@
-<%
-    String firstStr = request.getParameter("first");
-    String secondStr = request.getParameter("second");
-    String operation = request.getParameter("operation");
-    
-    int result=0;
-    boolean calcPerformed=false;
-    
-    if (operation!=null)
-    {
-        if (firstStr!=null && secondStr!=null && !firstStr.equals("") && !secondStr.equals(""))
-        {
-            int first = Integer.parseInt(firstStr);
-            int second = Integer.parseInt(secondStr);
-            
-            char operationType = operation.charAt(0);
-            
-            switch (operationType)
-            {
-                case '+': result = first + second;
-                    break;
-                case '-': result = first - second;
-                    break;
-                case '*': result = first * second;
-                    break;
-                case '%': result = first % second;
-                    break;
-            }
-            
-            calcPerformed = true;
-        }
-    }
-
-%>
-
-
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
@@ -51,6 +15,6 @@
             <input type="submit" name="operation" value="*"> <input type="submit" name="operation" value="%">
         </form>
         <br/>
-        RESULT: <%= (calcPerformed)?result:"---" %>
+        RESULT: ${result}
     </body>
 </html>
